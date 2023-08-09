a6734703ba6c1bc9646aa19b10ba3019c856b8b7
Paola <pao.ferdy04@gmail.com>
2023-08-08 22:05:13 -0600

    Adiccion de codigos

---
+++ Guia 1/ejemplo2/rectángulo.js
@@ -0,0 +1,4 @@
+var base = prompt('Introduzca la base:','');
+var altura = prompt('Introduzca la altura:','');
+area = (base*altura);
+document.write("<header><h1>El área del triangulo es: " + area + "</h1><hr /><br /></header>");
---
+++ Guia 1/ejemplo2/triangulo.js
@@ -0,0 +1,5 @@
+
+var base = prompt('Introduzca la base:','');
+var altura = prompt('Introduzca la altura:','');
+area = (base*altura)/2;
+document.write("<header><h1>El área del triangulo es: " + area + "</h1><hr /><br /></header>");
